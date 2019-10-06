f5e67802aaa125437f8f2e6755beda88f0ae6f0c
Jeff Spencer <jeffspencerd@gmail.com>
2019-10-06 00:19:55 -0400

    Add markdown settings for live preview with impatient mode

--- functions.el
+++ functions.el
@@ -4180,6 +4180,31 @@ The formatting is the same as is used with `format' function."
 	(org-refile-targets '((nil . (:maxlevel . 5)))))
     (org-refile)))
 
+(defun jj/markdown-html (buffer)
+  (princ (with-current-buffer buffer
+	   (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
+	 (current-buffer)))
+
+(defun jj/markdown-github-html (buffer)
+  (princ (with-current-buffer buffer
+	   (format "<!DOCTYPE html><html><script src=\"https://cdnjs.cloudflare.com/ajax/libs/he/1.1.1/he.js\"></script><link rel=\"stylesheet\" href=\"https://assets-cdn.github.com/assets/github-e6bb18b320358b77abe040d2eb46b547.css\"><link rel=\"stylesheet\" href=\"https://assets-cdn.github.com/assets/frameworks-95aff0b550d3fe338b645a4deebdcb1b.css\"><title>Impatient Markdown</title><div id=\"markdown-content\" style=\"display:none\">%s</div><div class=\"markdown-body\" style=\"max-width:968px;margin:0 auto;\"></div><script>fetch('https://api.github.com/markdown', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ \"text\": document.getElementById('markdown-content').innerHTML, \"mode\": \"gfm\", \"context\": \"knit-pk/homepage-nuxtjs\"}) }).then(response => response.text()).then(response => {document.querySelector('.markdown-body').innerHTML = he.decode(response)}).then(() => { fetch(\"https://gist.githubusercontent.com/FieryCod/b6938b29531b6ec72de25c76fa978b2c/raw/\").then(response => response.text()).then(eval)});</script></html>"
+		   (buffer-substring-no-properties (point-min) (point-max))))
+	 (current-buffer)))
+
+(defun jj/markdown-github-preview-impatient-mode ()
+  (interactive)
+  (impatient-mode 1)
+  (setq imp-user-filter #'jj/markdown-github-html)
+  (cl-incf imp-last-state)
+  (imp--notify-clients))
+
+(defun jj/markdown-preview-impatient-mode ()
+  (interactive)
+  (impatient-mode 1)
+  (setq imp-user-filter #'jj/markdown-html)
+  (cl-incf imp-last-state)
+  (imp--notify-clients))
+
 (defun jj/load-theme-sanityinc-tomorrow-eighties ()
   "Delete all themes, load theme eighties, setup smart-mode-line, and set the mode-line font"
   (interactive)
