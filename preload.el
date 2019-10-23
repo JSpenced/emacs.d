55310006df030f8687186de4dce7b87046516ad8
Jeff Spencer <jeffspencerd@gmail.com>
2019-10-22 22:47:18 -0400

    Rmove comments and update loading theme

--- preload.el
+++ preload.el
@@ -1,12 +1,4 @@
+;; TODO: Setting now in scimax to not load leuven theme
 ;; (when (display-graphic-p)
 ;;   (load-theme 'leuven t)
 ;;   )
-;; (setq desktop-path (list "~/Programs/scimax/user"))
-;; (desktop-change-dir "~/Programs/scimax/user")
-;; (desktop-save-mode 1)
-
-;; (setq emacs-lock-default-locking-mode 'kill)
-
-;; Setup initial buffer choice
-;; (setq initial-buffer-choice "~/Dropbox/Documents/Journal/Journal.org")
-;; (setq initial-buffer-choice "~/Dropbox/Documents/Journal/Learning-notes.org")
