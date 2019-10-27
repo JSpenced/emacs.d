752173467bea70efb0b8cd0588912995bd55663b
Jeff Spencer <jeffspencerd@gmail.com>
2019-10-27 16:27:05 -0400

    Remove custom.el from gitignore (mainly contains required packages)

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(transient zmq xterm-color swiper request projectile markdown-mode magit lv lsp-mode ivy hydra ht hl-todo helm-core helm ghub flycheck dash counsel company avy tree-mode vlf hideshow-org flymake-cursor zenburn-theme yaml-mode xah-lookup wttrin ws-butler writeroom-mode workgroups2 whole-line-or-region whitespace-cleanup-mode wgrep-ag wc-mode visual-regexp-steroids visible-mark use-package twilight-anti-bright-theme transpose-frame total-lines todotxt-mode todotxt tiny-menu tiny swiper-helm spacemacs-theme smex smartparens smart-mode-line sentence-navigation sensitive rainbow-mode rainbow-identifiers pydoc poporg peep-dired pdf-tools ov osx-trash osx-browse org-plus-contrib org-edna org-edit-latex org-download org-bullets openwith oauth noccur mustache multiple-cursors monokai-theme moe-theme magithub magit-todos magit-popup magit-gh-pulls macrostep lsp-ui load-theme-buffer-local lispy kill-or-bury-alive keychain-environment jupyter json-reformatter-jq json-navigator json-mode js2-mode jq-format jedi-direx ivy-youtube ivy-yasnippet ivy-xref ivy-hydra ivy-dired-history ibuffer-projectile hy-mode hungry-delete htmlize highlight-numbers helm-pydoc helm-projectile helm-gtags helm-dired-history helm-chronos helm-bibtex graphql google-translate google-this gitter github-review git-messenger git-commit-insert-issue ggtags function-args flyspell-correct-ivy flymake-json flycheck-demjsonlint flx flatland-theme flatland-black-theme eyebrowse expand-region evil-nerd-commenter evil esup elpy elfeed ejson-mode easy-kill-extras dumb-jump dtrt-indent drag-stuff dracula-theme dired-toggle-sudo dired-subtree dired-ranger dired-quick-sort dired-narrow dired-filter diminish deft dashboard darkroom cyphejor counsel-pydoc counsel-projectile counsel-gtags company-lsp color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-identifiers-mode clean-aindent-mode button-lock bug-hunter beacon backup-each-save back-button auctex-latexmk atom-one-dark-theme anti-zenburn-theme ample-zen-theme ample-theme all-the-icons-dired alect-themes aggressive-indent ag afternoon-theme ace-jump-zap ace-isearch)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
